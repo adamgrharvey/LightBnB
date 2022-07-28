3236fed1c14d3840e1c1ea643fa002d228623ddb
Adam <adamgrharvey@gmail.com>
2022-07-28 13:33:20 -0700

    adds Create Listing functionality using queries

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -72,10 +72,7 @@ const addUser =  function(user) {
       console.log(err.message);
       return (err.message);
     });
-  // const userId = Object.keys(users).length + 1;
-  // user.id = userId;
-  // users[userId] = user;
-  // return Promise.resolve(user);
+
 }
 exports.addUser = addUser;
 
@@ -207,24 +204,6 @@ exports.getAllReservations = getAllReservations;
   })
 };
 
-// SELECT properties.id, title, cost_per_night, avg(property_reviews.rating) as average_rating
-// FROM properties
-// LEFT JOIN property_reviews ON properties.id = property_id
-// WHERE city LIKE '%ancouv%'
-// GROUP BY properties.id
-// HAVING avg(property_reviews.rating) >= 4
-// ORDER BY cost_per_night
-// LIMIT 10;
-  
-  // return pool
-  // .query(`SELECT * FROM properties LIMIT $1`, [limit])
-  //   .then((result) => {
-  //     return (result.rows);
-  //   })
-  //   .catch((err) => {
-  //     return (err.message);
-  //   });
-
 exports.getAllProperties = getAllProperties;
 
 
@@ -234,9 +213,45 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const values = [
+  `${property.owner_id}`,
+  `${property.title}`,
+  `${property.description}`,
+  `${property.thumbnail_photo_url}`,
+  `${property.cover_photo_url}`,
+  `${property.cost_per_night}`,
+  `${property.parking_spaces}`,
+  `${property.number_of_bathrooms}`,
+  `${property.number_of_bedrooms}`,
+  `${property.country}`,
+  `${property.street}`,
+  `${property.city}`,
+  `${property.province}`,
+  `${property.post_code}`
+  ]
+  const queryString = `
+  INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code) 
+  VALUES (
+  $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *;
+  `;
+  return pool
+  .query(queryString, values)
+    .then((result) => {
+      console.log(result.rows[0])
+      return (result.rows[0]);
+    })
+    .catch((err) => {
+      console.log(err.message);
+      return (err.message);
+    });
+
+
+
+
+  // const propertyId = Object.keys(properties).length + 1;
+  // property.id = propertyId;
+  // properties[propertyId] = property;
+  // return Promise.resolve(property);
 }
 exports.addProperty = addProperty;
